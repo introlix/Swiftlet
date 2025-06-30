3a1544c4f528013ba5bd3a76d91c532bbe2f3973
Introlix <apibrains@gmail.com>
2025-06-30 21:44:06 +0545

    small error fixed

--- swiftlet/models/gemma/model_loader.py
+++ swiftlet/models/gemma/model_loader.py
@@ -8,17 +8,17 @@ class GemmaModelLoader:
     """
     A class to load the Gemma model from a specified path.
     """
-    def from_pretrained(self, model_path: str, map_location="cpu", quant: bool = False, quant_dtype: str = None):
+    def from_pretrained(self, model_path: str, map_location="cpu", quant: bool = False, quant_type: str = None):
 
-        if quant == True and quant_dtype is None:
+        if quant == True and quant_type is None:
             raise ValueError("If quant is True, quant_dtype must be specified (e.g., 'int8', 'int4', 'fp4')")
         
-        if quant == False and quant_dtype is not None:
+        if quant == False and quant_type is not None:
             quant = True
 
         if quant:
             self.quant = True
-            self.quant_dtype = quant_dtype
+            self.quant_type = quant_type
         
 
         def _collect_safetensors_files(path):
