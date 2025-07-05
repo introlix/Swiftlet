355f9eba13ab35f03dfe218bd8cd23315292c90d
Introlix <apibrains@gmail.com>
2025-07-05 17:24:09 +0545

    removed quant from embedding

--- swiftlet/kernels/embedding.py
+++ swiftlet/kernels/embedding.py
@@ -6,22 +6,22 @@ class Embedding(nn.Module):
 
     def __init__(self, num_embeddings: int, embedding_dim: int, quant: bool):
         super().__init__()
-        if quant:
-            self.weight = nn.Parameter(
-                torch.empty((num_embeddings, embedding_dim), dtype=torch.int8),
-                requires_grad=False,
-            )
-            self.weight_scaler = nn.Parameter(torch.Tensor(num_embeddings))
-        else:
-            self.weight = nn.Parameter(
-                torch.empty((num_embeddings, embedding_dim)),
-                requires_grad=False,
-            )
-        self.quant = quant
+        # if quant:
+        #     self.weight = nn.Parameter(
+        #         torch.empty((num_embeddings, embedding_dim), dtype=torch.int8),
+        #         requires_grad=False,
+        #     )
+        #     self.weight_scaler = nn.Parameter(torch.Tensor(num_embeddings))
+        # else:
+        self.weight = nn.Parameter(
+            torch.empty((num_embeddings, embedding_dim)),
+            requires_grad=False,
+        )
+        # self.quant = quant
 
     def forward(self, x):
         weight = self.weight
-        if self.quant:
-            weight = weight * self.weight_scaler.unsqueeze(-1)
+        # if self.quant:
+        #     weight = weight * self.weight_scaler.unsqueeze(-1)
         output = F.embedding(x, weight)
         return output
