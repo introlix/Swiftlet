1fb8fe53c4534a68f2136638b629024dd987a631
Introlix <apibrains@gmail.com>
2025-08-04 23:14:23 +0545

    added 7b support

--- swiftlet/models/qwen/config.py
+++ swiftlet/models/qwen/config.py
@@ -34,3 +34,30 @@ class QwenConfig:
 
 def get_config_for_1_5b_v2(tokenizer: Optional[str] = None, dtype: str = 'bfloat16') -> QwenConfig:
     return QwenConfig(dtype=dtype, tokenizer=tokenizer)
+
+def get_config_for_7b_v2(tokenizer: Optional[str] = None, dtype: str = 'bfloat16') -> QwenConfig:
+    return QwenConfig(
+        vocab_size=152064,
+        max_position_embeddings=131072,
+        num_hidden_layers=28,
+        num_attention_heads=28,
+        num_key_value_heads=4,
+        hidden_size=3584,
+        intermediate_size=18944,
+        head_dim=128,
+        hidden_act="silu",
+        rms_norm_eps=1e-6,
+        tokenizer=tokenizer,
+        use_bias=True,
+        attention_dropout=0.0,
+        bos_token_id=151643,
+        eos_token_id=151643,
+        initializer_range=0.02,
+        max_window_layers=28,
+        rope_theta=1000000.0,
+        sliding_window_size=131072,
+        tie_word_embeddings=False,
+        dtype=dtype,
+        use_cache=True,
+        use_sliding_window=False
+    )
